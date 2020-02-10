8c76738cf56eb1509f141a7fbb2d2bcf8bb4f554
MikhailSasnavik <mikhail.sasnavik@gmail.com>
2020-02-10 17:54:22 +0300

    xml attributes changed according to xml nodes' names

--- schedule/Scheduler.Business/Entities/DataLoad/Schedule.cs
+++ schedule/Scheduler.Business/Entities/DataLoad/Schedule.cs
@@ -11,15 +11,14 @@ namespace Scheduler.Business.Entities.DataLoad
     [XmlRoot("schedule")]
     public class Schedule
     {
-
-        [XmlArray("trains")]
-        [XmlArrayItem("train")]
-        public List<TrainRace> Trains {get; set;}
-        [XmlArray("buses")]
-        [XmlArrayItem("bus")]
+        [XmlArray("trainRaces")]
+        [XmlArrayItem("trainRace")]
+        public List<TrainRace> Trains { get; set; }
+        [XmlArray("busRaces")]
+        [XmlArrayItem("busRace")]
         public List<BusRace> Buses { get; set; }
-        [XmlArray("aircrafts")]
-        [XmlArrayItem("aircraft")]
+        [XmlArray("aircraftRaces")]
+        [XmlArrayItem("aircraftRace")]
         public List<AircraftRace> Aircrafts { get; set; }
     }
 }
